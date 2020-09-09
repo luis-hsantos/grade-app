44fe7058fc94171b9da1a41a612157a7f15ac7d0
Luís Fernandes <luis.hsantos@yahoo.com.br>
2020-09-09 20:34:52 -0300

    teste

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grade-api-app2020.herokuapp.com/',
+  baseURL: 'https://grade-api-app2020.herokuapp.com/grade/',
   headers: {
     'Content-type': 'application/json',
   },
